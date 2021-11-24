23cf0301f92b3a79f630cc05d0ec6403a33c926c
Victor Turitzin <turitzinv@gmail.com>
2021-11-24 18:12:58 -0500

    commit tree d6745ab99221874d10ebd3b19f71e67441d06d12

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case tip = 'generous' :
+      return 'Thank you so much.'
+  case tip = 'not as generous' :
+      return 'Thank you.'
+  default:
+      return 'Bye.';
+}
 }
